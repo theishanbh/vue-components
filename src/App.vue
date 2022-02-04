2c3813e586544c9edb321afb6ae22861f79be82f
ishanbh <theishanbh@gmail.com>
2022-02-04 23:02:36 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Greet name="Ishan" heroName="batman"/>
+  <Greet name="woah" heroName="superman"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Greet from './components/Greet.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Greet
   }
 }
 </script>
---
+++ src/components/Greet.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Hello {{name}}. your hero is {{heroName}} </div>
+</template>
+
+<script>
+export default {
+  name: 'Greet',
+  props: ['name','heroName']
+}
+</script>
+
